209dc09bdf258b67e0dd9c6ac7fdae0c2196f13f
JuergenKuck <drj.kuck@googlemail.com>
2025-03-10 16:47:59 +0100

    final

--- main.dart
+++ main.dart
@@ -1,13 +1,19 @@
 void main() {
-  for (int i = 0; i < 100; i++) {
-    int j = i + 1;
-    switch (j) {
-      case < 10:
-        print('Kleine Zahl: $j');
-      case >= 10 && < 60:
-        print('Mittlere Zahl: $j');
-      case >= 60:
-        print('Große Zahl: $j');
+  palimdrome("anna");
+  palimdrome("otto");
+  palimdrome("Test");
+}
+
+void palimdrome(String str) {
+  bool isPalindrome = true;
+  for (int i = 0; i < str.length; i++) {
+    if (str[i] != str[str.length - i - 1]) {
+      isPalindrome = false;
     }
+  }
+  if (isPalindrome) {
+    print("$str ist ein Palindrom.");
+  } else {
+    print("$str ist kein Palindrom.");
   }
 }
